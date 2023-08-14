e532a88803008e28b59158492d146f76a080e3d5
craygenx <marshmellowking99@gmail.com>
2023-08-14 13:34:45 +0300

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f'Hello, {name}!')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
 
 def add(num1, num2):
-    pass
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+    return result
 
 def halve(number):
-    pass
+    result = number / 2
+    print(result)
+    return result
+
+# greet_programmer()
+# greet()
+# greet_with_default()
